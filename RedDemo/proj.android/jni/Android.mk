97fee2275a91a09fce1437d9e6e45409cd0fecef
Ville Nukarinen <ville.nukarinen@gmail.com>
2015-05-16 21:12:26 +0300

    Added GameObject and Platform definitions to Android and CMake files

--- RedDemo/proj.android/jni/Android.mk
+++ RedDemo/proj.android/jni/Android.mk
@@ -12,9 +12,11 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
+                   ../../Classes/GameObject.cpp \
                    ../../Classes/GameScene.cpp \
                    ../../Classes/MenuScene.cpp \
-                   ../../Classes/PauseMenu.cpp
+                   ../../Classes/PauseMenu.cpp \
+                   ../../Classes/Platform.cpp
                    
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
